2575bf9c744e619252fe3d3dc60be03dfd3a4cc8
Wesley <whaakman@intercept.nl>
2020-11-20 19:57:41 +0100

    Initial commit

---
+++ AzFunction/SignupModel.cs
@@ -0,0 +1,12 @@
+namespace AzFunction
+{
+    public class SignupModel
+    {
+    public string Name { get; set; }
+    public string TwitterAccount { get; set; }
+    public string Email {get ; set; }
+    public string SolutionUri { get; set; }
+    public string Description { get; set; }
+    }
+}
+
