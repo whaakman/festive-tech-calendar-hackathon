33586dcc8f16ec42290a8808ed90bbcc211b443e
Wesley <whaakman@intercept.nl>
2020-11-22 00:44:01 +0100

    Added function to retrieve signups

---
+++ AzFunction/GetSignUps/SignupModel.cs
@@ -0,0 +1,11 @@
+namespace GetSignUps
+{
+    public class SignupModel
+    {
+    public string Name { get; set; }
+    public string TwitterAccount { get; set; }
+    public string Email {get ; set; }
+    public string SolutionUri { get; set; }
+    }
+}
+
